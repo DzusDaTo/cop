ccaa262738e21ab6738a5b9853735693cd45f8b5
datoshka <daviddzusov946@gmail.com>
2021-08-25 11:44:48 +0300

    добавление отправки тестов

--- testing/models.py
+++ testing/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Test(models.Model):
+    otvet1 = models.CharField('ФИО', max_length=50)
+    otvet2 = models.CharField('Ответ 2', max_length=5)
+    otvet3 = models.CharField('Ответ 3', max_length=5)
+    otvet4 = models.CharField('Ответ 4', max_length=5)
+    otvet5 = models.CharField('Ответ 5', max_length=5)
+
+    def __str__(self):
+        return self.otvet1
+
+    class Meta:
+        verbose_name = 'Тест'
+        verbose_name_plural = 'Тесты'
--- testing/views.py
+++ testing/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .forms import TestForm
 
 
 def testing(request):
-    return render(request, 'testing/testing.html')
+    error = ''
+    if request.method == 'POST':
+        form = TestForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            error = 'Форма была неверно заполнена'
+    form = TestForm()
+    data = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'testing/testing.html', data)
