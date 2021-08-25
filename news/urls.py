cc350e4d08437e029876ef9545f32c26a6437855
datoshka <daviddzusov946@gmail.com>
2021-08-25 11:08:59 +0300

    добавление функции вывод новостей

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,5 @@ from .import views
 
 urlpatterns = [
     path('', views.news_out),
+    path('<int:pk>', views.NewsDetailView.as_view(), name='news_detail')
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Articles
 
 
 def news_out(request):
-    return render(request, 'news/news_out.html')
+    news_home = Articles.objects.order_by('-date')
+    return render(request, 'news/news_out.html', {'news_home': news_home})
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/detail_view.html'
+    context_object_name = 'article'
+
+
+
